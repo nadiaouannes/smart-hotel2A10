95a0bd6506adb9e47e0b9258c500048de36dbfc8
onsdriss <48657242+onsdriss@users.noreply.github.com>
2020-04-09 21:59:15 +0200

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,59 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+#include "client.h"
+#include "reservation.h"
+#include <QMainWindow>
+
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_pb_ajouter_clicked();
+
+    void on_pb_supprimer_clicked();
+
+
+
+
+    void on_comboBox_currentIndexChanged(const QString &arg1);
+
+    void on_tabetudiant_activated(const QModelIndex &index);
+
+    void on_modifier_clicked();
+
+    void on_pb_ajouter_2_clicked();
+
+    void on_modifier_2_clicked();
+
+    void on_tabetudiant_2_activated(const QModelIndex &index);
+
+    void on_pb_supprimer_2_clicked();
+
+    void on_comboBox_2_currentIndexChanged(const QString &arg1);
+
+    void on_text_id_cursorPositionChanged(int arg1, int arg2);
+
+    void on_modifier_3_clicked();
+
+    void on_tmpclient_activated(const QModelIndex &index);
+
+    void on_tabreservation_activated(const QModelIndex &index);
+
+private:
+    Ui::MainWindow *ui;
+    Client tmpclient;
+    reservation tabreservation;
+
+};
+
+#endif // MAINWINDOW_H
